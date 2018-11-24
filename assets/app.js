a78eb8fc9c41dfe4b23a78006ff99faec72b22ef
Megan <mhmeluskey@gmail.com>
2018-11-23 20:03:05 -0500

    jquery changes

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,11 @@
 //what page looks like on load
 $(document).ready(function () {
-   
-        console.log("hello world")
-
-$("#button").on("click", function () {
-    $("#para").toggle('<img id = "mypic" class="responsive" src="images/profilepol.png" alt="mypic" height="40%" width="80%"></img>')
-});
-
- //for later use when portfolio is expanded upon  
-$("#contact").on("click", function () {
-    console.log("hello world");
-    $("#accordion").toggle("slow");
   
-    
-   
-    
-});
+console.log("hello world")
 
+$('.app-pic').delay(5000).fadeIn(2000);
 
-
-
-
+$('.project-links').delay(5000).fadeIn(2000);
 
 
 });
