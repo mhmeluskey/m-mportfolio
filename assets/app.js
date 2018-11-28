5bc685378e41afb177bf0b7b333c839b510a81fc
Megan <mhmeluskey@gmail.com>
2018-11-28 15:55:02 -0500

    more design

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,7 @@ $('.app-pic').delay(5000).fadeIn(2000);
 
 $('.project-links').delay(5000).fadeIn(2000);
 
-$('#philly-crop').delay(1000).fadeIn(3000);
+
+$('#web-developer').delay(1000).fadeIn(2000);
+$('#title').delay(500).fadeIn(2000);
 });
