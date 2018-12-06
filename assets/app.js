b87421d0ba01927e49e6d0cfaafb5110d243dcf8
Megan <mhmeluskey@gmail.com>
2018-12-05 20:43:17 -0500

    mobile responsive

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,4 @@
 //what page looks like on load
 $(document).ready(function() {
   console.log("hello world");
-
-  $(".app-pic")
-    .delay(1000)
-    .fadeIn(2000);
-
-  $("button")
-    .delay(1000)
-    .fadeIn(2000);
-  $("#myname")
-    .delay(500)
-    .fadeIn(2000);
 });
